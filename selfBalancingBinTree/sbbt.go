39ff7afd74898206749a7f2cb13d164d45d07982
omicron3069 <xlsgoodluck@gmail.com>
2018-12-08 17:55:00 +0800

    minor changes not enough coffee cant write code so it's minor changes

--- selfBalancingBinTree/sbbt.go
+++ selfBalancingBinTree/sbbt.go
@@ -7,16 +7,33 @@ import (
 func main() {
 	// the warehouse will play a major role in this program
 	var warehouse []*tree.Sucker
-	num := []int{3,2,1,4,5,6,7,10,9,8}
-	start(warehouse,num...)
+	start(warehouse, 3, 2, 1, 4, 5, 6, 7, 10, 9, 8)
 }
 
-func start( warehouse []*tree.Sucker,coco ...int) {
-	for _,v:=range coco{
-
+func start(warehouse []*tree.Sucker, coco ...int) {
+	var tmp *tree.Sucker
+	for _, v := range coco {
+		adder(warehouse, tmp, v)
+		tmp = nil
 	}
 }
 
-func balanceCheck(warehouse []*tree.Sucker) bool {
-	
+func adder(warehouse []*tree.Sucker, goods *tree.Sucker, value int) {
+	// TODO: error handling
+	var (
+		pos tree.Born
+		up  *tree.Sucker
+	)
+	vCheck := func(current *tree.Sucker) tree.Born {
+		switch {// complex logical operation goes here
+
+		}
+		return tree.NULL
+	}
+	warehouse[0].Travel(vCheck)
+	goods.InitNode(value, up, pos)
+}
+
+func balanceCheck(warehouse []*tree.Sucker) {
+	// TODO: error handling
 }
--- selfBalancingBinTree/tree/tree.go
+++ selfBalancingBinTree/tree/tree.go
@@ -2,55 +2,59 @@ package tree
 
 // define a simple tree struct
 type Sucker struct {
-	value                  int
-	parent, lChild, rChild *Sucker
+	Value                  int
+	Parent, LChild, RChild *Sucker
 }
 
 // this thing are kind like enum in C/C++
-type born int
+type Born int
 
 const (
-	LEFT  born = 0
-	RIGHT born = 1
+	LEFT  Born = 0
+	RIGHT Born = 1
+	NULL  Born = 9
 )
 
-// plug this node into it's parent's arm
-func (s *Sucker) InitNode(num int, up *Sucker, local born) {
-	s.value = num
-	s.parent = up
+// plug this node into it's Parent's arm
+func (s *Sucker) InitNode(num int, up *Sucker, local Born) {
+	s.Value = num
+	s.Parent = up
 	// as mentioned in golang tour
 	// use switch to replace else if
 	switch {
 	case local == LEFT:
-		up.lChild = s
+		up.LChild = s
 	case local == RIGHT:
-		up.rChild = s
+		up.RChild = s
 		// TODO : finish error message after learned error handling
 	}
 	// set it to empty
-	s.rChild, s.lChild = nil, nil
+	s.RChild, s.LChild = nil, nil
 }
 
 // use recursive method Travel all node
 // hand function can
-func (s *Sucker) Travel(hand func(*Sucker) born) {
+func (s *Sucker) Travel(hand func(*Sucker) Born) {
 	chick := hand(s)
 	switch {
 	case chick == LEFT:
 		// left, then right
-		if s.lChild.isNotNil() {
-			s.lChild.Travel(hand)
+		if s.LChild.isNotNil() {
+			s.LChild.Travel(hand)
 		}
-		if s.rChild.isNotNil() {
-			s.rChild.Travel(hand)
+		if s.RChild.isNotNil() {
+			s.RChild.Travel(hand)
 		}
 	case chick == RIGHT:
-		if s.rChild.isNotNil() {
-			s.rChild.Travel(hand)
+		if s.RChild.isNotNil() {
+			s.RChild.Travel(hand)
 		}
-		if s.lChild.isNotNil() {
-			s.lChild.Travel(hand)
+		if s.LChild.isNotNil() {
+			s.LChild.Travel(hand)
 		}
+	case chick == NULL:
+		// do nothing, continue recursive travel
+
 		// TODO: I need an error handler to fill "default", might same as above
 	}
 }
